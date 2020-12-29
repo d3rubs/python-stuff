164d41b34892d14608d0ce49cb659a498b7c2e9b
d3rubs <d3rubs@gmail.com>
2020-12-28 17:44:37 -0800

    new file

---
+++ new-project2020.12.28/main.py
@@ -0,0 +1,8 @@
+# rss
+
+tolerance = .005
+dimension_1 = input("Enter first dimension ")
+dimension_2 = input("Enter second dimension ")
+float(dimension_1)
+float(dimension_2)
+print(dimension_1 + dimension_2)
